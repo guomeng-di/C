0c548d3eb8f845f7b630a6750eef9eb8163989f4
郭梦迪 <yjzty222@qq.com>
2025-11-04 20:25:15 +0800

    Add files via upload

---
+++ 3-4.c
@@ -0,0 +1,11 @@
+//3-4
+#include <stdio.h>
+int main(){
+float a;
+printf("Enter a floating-point value:");
+scanf("%f",&a);
+printf("fixed-point notation: %f\n",a);
+printf("exponential notation: %e\n",a);
+printf("p notation: %a\n",a);
+  return 0;
+}
